76fa8579a2af470543ed2ae6c3748ac660682d35
Ajou201421102 <sktvudghk@gmail.com>
2021-01-19 23:02:58 +0900

    Update my code

--- test_baseball_game.py
+++ test_baseball_game.py
@@ -3,7 +3,7 @@
 import unittest
 import baseball_game as bg
 
-from mock import patch
+from unittest.mock import patch
 from io import StringIO
 
 
@@ -230,4 +230,7 @@ class TestBaseballGame(unittest.TestCase):
                 else:
                     ball += 1
         result = [strikes, ball]
+
         return result
+# if __name__ == "__main__":
+#     unittest.main() 
